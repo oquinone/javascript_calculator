6b0a5ac977c7ac275bafc6b74ad7beb92c430c3c
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-02-10 14:06:10 -0800

    Updates

---
+++ precache-manifest.25a416895e1b8aece476e3b7570c49cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5d5906c634a2ce22cae02067a249dd",
+    "url": "/oquinone/javascript_calculator/index.html"
+  },
+  {
+    "revision": "367a0a103dd0cbfa296a",
+    "url": "/oquinone/javascript_calculator/static/css/main.c9d676a8.chunk.css"
+  },
+  {
+    "revision": "b47a90190df1a07600e3",
+    "url": "/oquinone/javascript_calculator/static/js/2.1b634478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oquinone/javascript_calculator/static/js/2.1b634478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367a0a103dd0cbfa296a",
+    "url": "/oquinone/javascript_calculator/static/js/main.62663774.chunk.js"
+  },
+  {
+    "revision": "a0186f03caae0ed8397b",
+    "url": "/oquinone/javascript_calculator/static/js/runtime-main.4ef7389b.js"
+  }
+]);
